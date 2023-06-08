fb082985fbc7a9221b438861f5cb8833beea40f0
Mohamed Aliyar <123193915+MOHAMED-ALIYAR@users.noreply.github.com>
2023-06-08 23:42:11 +0530

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 import pickle
 import string
+from PIL import Image
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
@@ -65,6 +66,22 @@ mnb.fit(X_train,y_train)
 
 
 st.title("Spam Message Classifier")
+
+def img(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+        unsafe_allow_html=True
+    )
+img('Images/bg.jpg')
 
 input_sms = st.text_area("Enter the message")
 
